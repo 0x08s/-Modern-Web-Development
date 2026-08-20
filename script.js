fe6d6fdb69d0958bf0e8976000e40b7e48345a2e
sawera <241689067+echomesawera@users.noreply.github.com>
2026-08-20 20:21:24 +0500

    Added web development files from Kali Linux Desktop

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = "Sawera";
+
+let b = 14 ;
+
+console.log(a);
+console.log(b);
+
+let city = "Lahore";
+
+console.log(city);
+console.log(typeof city);
+
+let name = "Sawera";
+let age = 17;
+let isstudent = Boolean;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isstudent);
